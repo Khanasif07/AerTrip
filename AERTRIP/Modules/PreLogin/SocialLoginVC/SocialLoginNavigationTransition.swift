1983ccc3a3e97a91a8b4ae898502a0fc862f47fa
Pramod Kumar <pramod.kumar@appinventiv.com>
2018-12-29 17:10:53 +0530

    working on nav trans

--- AERTRIP/Modules/PreLogin/SocialLoginVC/SocialLoginNavigationTransition.swift
+++ AERTRIP/Modules/PreLogin/SocialLoginVC/SocialLoginNavigationTransition.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2018 Pramod Kumar. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+open class SocialLoginNavigationTransition: NSObject {
+    
+    open var duration = 0.5
+    
+    open var transitionMode: ATTransitionMode = .present
+
+}
+
+extension SocialLoginNavigationTransition: UIViewControllerAnimatedTransitioning {
+    
+    // MARK: - UIViewControllerAnimatedTransitioning
+    
+    /**
+     Required by UIViewControllerAnimatedTransitioning
+     */
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return duration
+    }
+    
+    /**
+     Required by UIViewControllerAnimatedTransitioning
+     */
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        let containerView = transitionContext.containerView
+        
+        let fromViewController = transitionContext.viewController(forKey: .from)
+        let toViewController = transitionContext.viewController(forKey: .to)
+        
+    }
+}
