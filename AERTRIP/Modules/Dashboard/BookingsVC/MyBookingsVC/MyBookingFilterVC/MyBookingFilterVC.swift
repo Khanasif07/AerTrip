ce88e16cf7272846ea139cbb10e5a2cf513a1bc3
Pramod Kumar <pramod.kumar@appinventiv.com>
2019-07-26 17:55:34 +0530

    final change for category

--- AERTRIP/Modules/Dashboard/BookingsVC/MyBookingsVC/MyBookingFilterVC/MyBookingFilterVC.swift
+++ AERTRIP/Modules/Dashboard/BookingsVC/MyBookingsVC/MyBookingFilterVC/MyBookingFilterVC.swift
@@ -13,22 +13,11 @@ class MyBookingFilterVC: BaseVC {
     //MARK:- Variables
     //MARK:- Private
     private var currentIndex: Int = 0
-//    fileprivate weak var categoryView: ATCategoryView!
     private let allTabsStr: [String] = [LocalizedString.TravelDate.localized, LocalizedString.EventType.localized, LocalizedString.BookingDate.localized]
     private var allTabButtons: [PKCategoryButton] {
         return [travelDateButton, eventTypeButton, bookingDateButton]
     }
-//    private var allTabs: [ATCategoryItem] {
-//        var temp = [ATCategoryItem]()
-//        for title in self.allTabsStr {
-//            var obj = ATCategoryItem()
-//            obj.title = title
-//            temp.append(obj)
-//        }
-//        return temp
-//    }
-    
-//    private var allChildVCs: [UIViewController] = [UIViewController]()
+
     private var minDate: Date?
     
     private var travelDateVC: TravelDateVC?
@@ -71,69 +60,10 @@ class MyBookingFilterVC: BaseVC {
         
         self.mainContainerView.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 10.0)
         self.edgesForExtendedLayout = UIRectEdge(rawValue: 4)
-        
-//        for i in 0..<self.allTabsStr.count {
-//            if i == 0 {
-//                if let vc = AppGlobals.shared.travelDateVC {
-//                    self.allChildVCs.append(vc)
-//                }
-//                else {
-//                    let vc = TravelDateVC.instantiate(fromAppStoryboard: .Bookings)
-//                    vc.delegate = self
-//                    vc.minFromDate = minDate
-//                    vc.oldFromDate = MyBookingFilterVM.shared.travelFromDate
-//                    vc.oldToDate = MyBookingFilterVM.shared.travelToDate
-//                    vc.currentlyUsingAs = .travelDate
-//
-//                    //                    AppGlobals.shared.travelDateVC = vc
-//
-//                    self.allChildVCs.append(vc)
-//                }
-//
-//            } else if i == 1 {
-//                if let vc = AppGlobals.shared.eventTypeVC {
-//                    self.allChildVCs.append(vc)
-//                }
-//                else {
-//                    let vc = EventTypeVC.instantiate(fromAppStoryboard: .Bookings)
-//                    vc.delegate = self
-//                    if MyBookingFilterVM.shared.isFirstTime {
-//                        vc.oldSelection = []
-//                    } else {
-//                        vc.oldSelection = MyBookingFilterVM.shared.eventType
-//                    }
-//                    //if all event types wan to show as deselected by-default
-//                    //vc.oldSelection = MyBookingFilterVM.shared.eventType //if all event types wan to show as selected by-default
-//
-//                    //                    AppGlobals.shared.eventTypeVC = vc
-//
-//
-//
-//                    self.allChildVCs.append(vc)
-//                }
-//
-//            } else {
-//                if let vc = AppGlobals.shared.bookingDateVC {
-//                    self.allChildVCs.append(vc)
-//                }
-//                else {
-//                    let vc = TravelDateVC.instantiate(fromAppStoryboard: .Bookings)
-//                    vc.delegate = self
-//                    vc.minFromDate = minDate
-//                    vc.oldFromDate = MyBookingFilterVM.shared.bookingFromDate
-//                    vc.oldToDate = MyBookingFilterVM.shared.bookingToDate
-//                    vc.currentlyUsingAs = .bookingDate
-//
-//                    //                    AppGlobals.shared.bookingDateVC = vc
-//
-//                    self.allChildVCs.append(vc)
-//                }
-//            }
-//        }
-        
+
         let height = UIApplication.shared.statusBarFrame.height
         self.navigationViewTopConstraint.constant = CGFloat(height)
-//        self.setupPagerView()
+
         self.configureTabBar()
         self.hide(animated: false)
         delay(seconds: 0.01) { [weak self] in
@@ -141,13 +71,7 @@ class MyBookingFilterVC: BaseVC {
         }
         self.setupGesture()
     }
-    
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        self.categoryView?.frame = self.childContainerView.bounds
-//        self.categoryView?.layoutIfNeeded()
-//    }
-    
+
     override func setupTexts() {
     }
     
@@ -166,7 +90,6 @@ class MyBookingFilterVC: BaseVC {
     }
     private func notifyToFilterApplied() {
         
-//        self.setBadgesOnAllCategories()
         self.setBadge()
         
         NSObject.cancelPreviousPerformRequests(withTarget: self)
@@ -215,64 +138,6 @@ class MyBookingFilterVC: BaseVC {
             }
         })
     }
-    
-    
-//    private func setupPagerView() {
-//        var style = ATCategoryNavBarStyle()
-//        style.height = 50.0
-//        style.interItemSpace = 21.8
-//        style.itemPadding = 12.8
-//        style.isScrollable = false
-//        style.layoutAlignment = .left
-//        style.isEmbeddedToView = true
-//        style.showBottomSeparator = true
-//        style.bottomSeparatorColor = AppColors.themeGray40
-//        style.defaultFont = AppFonts.Regular.withSize(16.0)
-//        style.selectedFont = AppFonts.SemiBold.withSize(16.0)
-//        style.indicatorColor = AppColors.themeGreen
-//        style.normalColor = AppColors.textFieldTextColor51
-//        style.selectedColor = AppColors.themeBlack
-//
-//        style.badgeDotSize = CGSize(width: 4.0, height: 4.0)
-//        style.badgeBackgroundColor = AppColors.themeGreen
-//        style.badgeBorderColor = AppColors.clear
-//        style.badgeBorderWidth = 0.0
-//
-//
-//        let categoryView = ATCategoryView(frame: self.childContainerView.bounds, categories: self.allTabs, childVCs: self.allChildVCs, parentVC: self, barStyle: style)
-//        categoryView.interControllerSpacing = 0.0
-//        categoryView.navBar.internalDelegate = self
-//        self.childContainerView.addSubview(categoryView)
-//        self.categoryView = categoryView
-//
-//        // Set last Selected Index on Nav bar
-//        self.categoryView.select(at: MyBookingFilterVM.shared.lastSelectedIndex)
-//        self.setBadgesOnAllCategories()
-//    }
-    
-//    private func setBadgesOnAllCategories() {
-//
-//        for (idx,tab) in self.allTabsStr.enumerated() {
-//
-//            var badgeCount = 0
-//
-//            switch tab.lowercased() {
-//            case LocalizedString.TravelDate.localized.lowercased():
-//                badgeCount = (MyBookingFilterVM.shared.travelToDate != nil || MyBookingFilterVM.shared.travelFromDate != nil) ? 1 : 0
-//
-//            case LocalizedString.EventType.localized.lowercased():
-//                badgeCount = MyBookingFilterVM.shared.eventType.isEmpty ? 0 : 1
-//
-//            case LocalizedString.BookingDate.localized.lowercased():
-//                badgeCount = (MyBookingFilterVM.shared.bookingToDate != nil || MyBookingFilterVM.shared.bookingFromDate != nil) ? 1 : 0
-//
-//            default:
-//                printDebug("not useable case")
-//            }
-//
-//            self.categoryView.setBadge(atIndex: idx, badgeNumber: badgeCount)
-//        }
-//    }
     
     private func setupGesture() {
         let gestureRecognizer = UITapGestureRecognizer(target: self,action: #selector(outsideAreaTapped))
@@ -527,7 +392,6 @@ extension MyBookingFilterVC {
             newX = xFor(index: fromIndex)
         }
         
-//        print("fromIndex: \(newX)")
         let indicatorFrame = CGRect(x: newX, y: self.allTabTitleContainerView.height - indicatorHeight, width: indicatorWidth, height: indicatorHeight)
         
         self.tabSelectionIndicatorView.frame = indicatorFrame
