651179effbe35cf119efea845d07b05c2794e5cf
@sudhir <sudhir.pal@appinventiv.com>
2019-10-08 12:34:50 +0530

    guest name check added

--- AERTRIP/Modules/Dashboard/HotelsVC/HotelCheckoutVC/HCDataSelectionVC/HCDataSelectionVC+Cells.swift
+++ AERTRIP/Modules/Dashboard/HotelsVC/HotelCheckoutVC/HCDataSelectionVC/HCDataSelectionVC+Cells.swift
@@ -244,7 +244,7 @@ class HCDataSelectionRoomDetailsCollectionCell: UICollectionViewCell {
                 setupForAdd()
             }
             else {
-                infoImageView.isHidden = !((fName.isEmpty || fName.count <= 4) || (lName.isEmpty || lName.count <= 4) || saltn.isEmpty)
+                infoImageView.isHidden = !((fName.isEmpty || fName.count < 3) || (lName.isEmpty || lName.count < 3) || saltn.isEmpty)
                 titleLabel.text = self.contact?.fullName
                 if let img = self.contact?.profilePicture, !img.isEmpty {
                     self.iconImageView.setImageWithUrl(img, placeholder: placeHolder, showIndicator: false)
