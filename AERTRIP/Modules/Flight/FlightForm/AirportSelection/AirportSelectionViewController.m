73262498b1b391ad1807d7191d6d9652a5c6d5bf
Rishabh Nautiyal <rishabh.nautiyal@appinventiv.com>
2020-07-28 18:55:41 +0530

    Airport result table blink issue

--- AERTRIP/Modules/Flight/FlightForm/AirportSelection/AirportSelectionViewController.m
+++ AERTRIP/Modules/Flight/FlightForm/AirportSelection/AirportSelectionViewController.m
@@ -177,9 +177,9 @@
 
         
         if (self.viewModel.isFrom){
-            [self fromAction:nil];
+//            [self fromAction:nil];
         }else {
-            [self toAction:nil];
+//            [self toAction:nil];
         }
         
     } failure:^(NSString *error, BOOL popup) {
@@ -246,7 +246,7 @@
     [self setupTableView];
     self.airportDisplayArray = [[NSMutableArray alloc] init];
     self.displaySections = [[NSMutableDictionary alloc] init];
-    [self refreshAllUIElements];
+    [self refreshAllUIElements:false];
     [self performNearbyAirportsSearch];
     [self notifications];
     [self showNoResultView];
@@ -463,7 +463,7 @@
     
     self.airportDisplayArray = [[NSMutableArray alloc] init];
     self.displaySections = [[NSMutableDictionary alloc] init];
-    [self refreshAllUIElements];
+    [self refreshAllUIElements:true];
 }
 
 - (void)updateUIAfterAddingAirport {
@@ -522,14 +522,14 @@
     if ([[Parser getValueForKey:@"type" inDictionary:dataDictionary] isEqualToString:@"airports"]) {
         self.cellIdentifier = @"AirportCell";
         [self createAirportArrayFromSearchResult:[Parser parseAirportSearchArray:[dataDictionary objectForKey:@"results"]]];
-        [self refreshAllUIElements];
+        [self refreshAllUIElements:true];
         return;
     }
     
     if ([[Parser getValueForKey:@"type" inDictionary:dataDictionary] isEqualToString:@"airlines"]) {
         self.cellIdentifier = @"AirlineSearchCell";
         [self createAirlineArrayFromSearchResult:[dataDictionary objectForKey:@"results"]];
-        [self refreshAllUIElements];
+        [self refreshAllUIElements:true];
         return;
     }
     
@@ -784,7 +784,7 @@
 }
 
 
-- (void)refreshAllUIElements {
+- (void)refreshAllUIElements:(BOOL)shouldReloadTable {
     
     if ( self.viewModel.toFlightArray.count == 0 && self.viewModel.fromFlightArray.count == 0 ) {
         [self.switcherButton setEnabled:false];
@@ -794,7 +794,9 @@
     
     [self populateDisplayArrays];
     [self setupFromAndToView];
-    [self.resultTableView reloadData];
+    if (shouldReloadTable) {
+        [self.resultTableView reloadData];
+    }
     [self showNoResultView];
     [self hideLoader:YES];
     
@@ -1702,7 +1704,7 @@
     self.searchBar.text = @"";
     self.airportDisplayArray = [[NSMutableArray alloc] init];
     self.displaySections = [[NSMutableDictionary alloc] init];
-    [self refreshAllUIElements];
+    [self refreshAllUIElements:false];
     [self performNearbyAirportsSearch];
 }
 
@@ -1714,7 +1716,7 @@
     self.searchBar.text = @"";
     self.airportDisplayArray = [[NSMutableArray alloc] init];
     self.displaySections = [[NSMutableDictionary alloc] init];
-    [self refreshAllUIElements];
+    [self refreshAllUIElements:false];
     [self performNearbyAirportsSearch];
 }
 
