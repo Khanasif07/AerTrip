0af8ec860773f45aa580ebb9cd57e00744a13672
Gurpreet Singh <gurpreet.singh@appinventiv.com>
2020-08-20 18:14:15 +0530

    Removed filters applied from vc

---
+++ AERTRIP/Modules/Flight/FilghtResults/FlightResultSingle/FlightResultSingleJourneyVC+DataReceivingFlow.swift
@@ -0,0 +1,9 @@
+//
+//  FlightResultSingleJourneyVC+DataReceivingFlow.swift
+//  AERTRIP
+//
+//  Created by Appinventiv on 20/08/20.
+//  Copyright © 2020 Pramod Kumar. All rights reserved.
+//
+
+import Foundation
