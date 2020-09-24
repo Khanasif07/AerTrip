f1ec4ad0f72b670c8d37884f6a167c93b5c50267
Rishabh Nautiyal <rishabh.nautiyal@appinventiv.com>
2020-09-24 16:30:42 +0530

    Deep-linking filters ui update for domestic return/multi-city

--- AERTRIP/Modules/Flight/FilghtResults/FlightResultHome/FlightResultBaseViewController.swift
+++ AERTRIP/Modules/Flight/FilghtResults/FlightResultHome/FlightResultBaseViewController.swift
@@ -1189,19 +1189,22 @@ extension FlightResultBaseViewController  : FlightResultViewModelDelegate , NoRe
         if isAPIResponseUpdated {
             // for other searches except ones mentioned below
             self.flightFilterVC?.flightResultArray = self.flightSearchResultVM.flightResultArray
-            flightFilterVC?.appliedAndUIFilters =  flightSearchResultVM.flightLegsAppliedFilters
             DispatchQueue.main.async {
+                self.flightFilterVC?.appliedAndUIFilters =  self.flightSearchResultVM.flightLegsAppliedFilters
                 self.flightFilterVC?.userSelectedFilters = self.flightSearchResultVM.getUserSelectedFilters()
                 self.flightFilterVC?.updateInputFilters(flightResultArray: self.flightSearchResultVM.flightResultArray)
             }
-//            flightFilterVC?.userSelectedFilters = self.flightSearchResultVM.getUserSelectedFilters()
-//            self.flightFilterVC?.updateInputFilters(flightResultArray: self.flightSearchResultVM.flightResultArray)
             
+            // For updating UI from deep linking filters // might not get set at the first time
+            DispatchQueue.delay(0.2, closure: {
+                self.flightFilterVC?.updateInputFilters(flightResultArray: self.flightSearchResultVM.flightResultArray)
+            })
             
-            // for international return and multicity
             self.intMCAndReturnFilterVC?.flightResultArray = self.flightSearchResultVM.intFlightResultArray
-            intMCAndReturnFilterVC?.appliedAndUIFilters = flightSearchResultVM.intFlightLegsAppliedFilters
+            DispatchQueue.main.async {
+                self.intMCAndReturnFilterVC?.appliedAndUIFilters = self.flightSearchResultVM.intFlightLegsAppliedFilters
             self.intMCAndReturnFilterVC?.updateInputFilters(flightResultArray: self.flightSearchResultVM.intFlightResultArray)
+            }
             
             // To check if filters are pre applied and update dots
             filtersApplied(true)
