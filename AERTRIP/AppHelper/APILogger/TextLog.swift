ed0144316bdcae0ccc361d3358595f7fe76207c4
Nitin Singh <nitin.singh@appinventiv.com>
2020-11-03 23:03:35 +0530

    # Incorrect animation for long press and cancel -> in progress # Select guests: (Common with Hotels) -> Not feasible # Card getting cut at the bottom on iPhone SE -> Main page issue # Recent searches is not scrolling properly on iPhone SE -> Working # Don't show recent searches when there are no recent searches -> Done

--- AERTRIP/AppHelper/APILogger/TextLog.swift
+++ AERTRIP/AppHelper/APILogger/TextLog.swift
@@ -12,23 +12,23 @@ struct TextLog: TextOutputStream {
 
     mutating func write(_ string: String) {
         
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask)
-        let documentDirectoryPath = paths.first!
-        let log = documentDirectoryPath.appendingPathComponent("log.txt")
-        print("log= ",log)
-        do {
-            let handle = try FileHandle(forWritingTo: log)
-            handle.seekToEndOfFile()
-            handle.write(string.data(using: .utf8)!)
-            handle.closeFile()
-        } catch {
-            print(error.localizedDescription)
-            do {
-                try string.data(using: .utf8)?.write(to: log)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask)
+//        let documentDirectoryPath = paths.first!
+//        let log = documentDirectoryPath.appendingPathComponent("log.txt")
+//        print("log= ",log)
+//        do {
+//            let handle = try FileHandle(forWritingTo: log)
+//            handle.seekToEndOfFile()
+//            handle.write(string.data(using: .utf8)!)
+//            handle.closeFile()
+//        } catch {
+//            print(error.localizedDescription)
+//            do {
+//                try string.data(using: .utf8)?.write(to: log)
+//            } catch {
+//                print(error.localizedDescription)
+//            }
+//        }
     }
 }
 
