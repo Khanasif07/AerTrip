af2fea7375ca8d5c0f4e06a232de1dbdb0dcc021
Monika <monika.sonawane@aertrip.com>
2020-11-05 16:29:06 +0530

    blank commit

--- AERTRIP/Modules/Flight/FilghtResults/FlightDomesticMultiLegResultVC/FlightDomesticMultiLegResultVC+Properties.swift
+++ AERTRIP/Modules/Flight/FilghtResults/FlightDomesticMultiLegResultVC/FlightDomesticMultiLegResultVC+Properties.swift
@@ -139,7 +139,7 @@ class FlightDomesticMultiLegResultVC: UIViewController , NoResultScreenDelegate,
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
+        super.viewWillAppear(animated)
         
         self.emailPinnedFlights.setImage(UIImage(named: "EmailPinned"), for: .normal)
         self.emailPinnedFlights.displayLoadingIndicator(false)
