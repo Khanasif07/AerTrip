245617aa0ed7943ce264da977e710150f49c7330
Admin <gurpreet.singh@appinventiv.com>
2021-01-21 19:55:59 +0530

    blank commit

--- AERTRIP/Modules/Version Controler/VersionControler.swift
+++ AERTRIP/Modules/Version Controler/VersionControler.swift
@@ -24,7 +24,6 @@ class VersionControler {
                 
         APICaller.shared.checkFOrUpdates(params: params) { (data, codes) in
             
-         
             guard let version = data else { return }
             if version.latestVersion == version.installedVersion { return }
             
