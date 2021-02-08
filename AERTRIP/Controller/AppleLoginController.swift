a65cce6a02c758229fe1a9f02ec4b881845db6f5
Rishabh <rishabh.nautiyal@appinventiv.com>
2021-02-08 14:23:36 +0530

    Version control cehck and apple sign in issue

--- AERTRIP/Controller/AppleLoginController.swift
+++ AERTRIP/Controller/AppleLoginController.swift
@@ -44,12 +44,25 @@ class AppleLoginController : NSObject, ASAuthorizationControllerDelegate {
             let name = appleIDCredential.fullName
             let firstName = name?.givenName ?? ""
             let lastName = name?.familyName ?? ""
-            let email = appleIDCredential.email ?? ""
+            var email = appleIDCredential.email ?? ""
             let fullName = "\(firstName) \(lastName)"
+            if let idTokenData = appleIDCredential.identityToken {
+                do {
+                    if let idTokenStr = String(data: idTokenData, encoding: .utf8) {
+                        let dataDict = try decode(jwtToken: idTokenStr)
+                        if let emailId = dataDict["email"] as? String {
+                            email = emailId
+                        }
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+            
             printDebug(appleIDCredential.identityToken)
             printDebug("User id is \(userIdentifier) \n Full Name is \(String(describing: fullName)) \n Email id is \(String(describing: email))")
-
-           let user =  AppleUser(id: userIdentifier, fullName: fullName, firstName: firstName, lastName: lastName, email: email)
+            
+            let user =  AppleUser(id: userIdentifier, fullName: fullName, firstName: firstName, lastName: lastName, email: email)
             success?(user)
             success = nil
             failure = nil
@@ -62,6 +75,34 @@ class AppleLoginController : NSObject, ASAuthorizationControllerDelegate {
         failure?(error)
         success = nil
         failure = nil
+    }
+    
+    private func decode(jwtToken jwt: String) throws -> [String: Any] {
+        
+        enum DecodeErrors: Error {
+            case badToken
+            case other
+        }
+        
+        func base64Decode(_ base64: String) throws -> Data {
+            let padded = base64.padding(toLength: ((base64.count + 3) / 4) * 4, withPad: "=", startingAt: 0)
+            guard let decoded = Data(base64Encoded: padded) else {
+                throw DecodeErrors.badToken
+            }
+            return decoded
+        }
+        
+        func decodeJWTPart(_ value: String) throws -> [String: Any] {
+            let bodyData = try base64Decode(value)
+            let json = try JSONSerialization.jsonObject(with: bodyData, options: [])
+            guard let payload = json as? [String: Any] else {
+                throw DecodeErrors.other
+            }
+            return payload
+        }
+        
+        let segments = jwt.components(separatedBy: ".")
+        return try decodeJWTPart(segments[1])
     }
     
 }
