fe9241f334e9eb1fdd1b39e34f4c56df2f55356e
Gurpreet <gurpreet.singh@appinventiv.com>
2021-04-05 12:03:30 +0530

    blank commit

--- AERTRIP/Modules/Dashboard/BookingsVC/MyBookingsVC/MyBookingsVC.swift
+++ AERTRIP/Modules/Dashboard/BookingsVC/MyBookingsVC/MyBookingsVC.swift
@@ -27,6 +27,7 @@ class MyBookingsVC: BaseVC {
         return UIApplication.shared.isStatusBarHidden ? CGFloat(0) : UIApplication.shared.statusBarFrame.height
     }
     
+    
     private var time: Float = 0.0
     private var timer: Timer?
     
