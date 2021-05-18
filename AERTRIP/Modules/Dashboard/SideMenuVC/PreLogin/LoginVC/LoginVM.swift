ede8b594430fd135a80d1eb544fe0a0ca891daba
Golu Kumar <golu.kumar@appinventiv.com>
2021-05-18 11:06:00 +0530

    code update

--- AERTRIP/Modules/Dashboard/SideMenuVC/PreLogin/LoginVC/LoginVM.swift
+++ AERTRIP/Modules/Dashboard/SideMenuVC/PreLogin/LoginVC/LoginVM.swift
@@ -30,6 +30,8 @@ class LoginVM {
             return false
         } else if self.password.isEmpty {
             return false
+        }else if !(self.password.isValidPasswordCharacterCount){
+            return false
         }
 //        else if self.password.checkInvalidity(.Password) {
 //           return false
