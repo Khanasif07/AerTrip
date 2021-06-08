c326faef6e94ae1115e1e553d5425611429b4b8b
Rishabh <rishabh.nautiyal@appinventiv.com>
2021-06-08 15:14:36 +0530

    Picker font

--- AERTRIP/Modules/Flight/FlightForm/FlightAddPassenger/FlightAddPassengerViewController.m
+++ AERTRIP/Modules/Flight/FlightForm/FlightAddPassenger/FlightAddPassengerViewController.m
@@ -217,8 +217,23 @@ int alreadySelectedComponent = 0;
     self.doneButton.enabled = YES;
 }
 
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    
+//- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+//
+//    NSString * stringForTitle;
+//    if (component == 0) {
+//        stringForTitle = [self.numberOfAdultArray objectAtIndex:row];
+//    }else if (component == 1) {
+//        stringForTitle =  [self.numberOfChildArray objectAtIndex:row];
+//    }else if (component == 2) {
+//        stringForTitle = [self.numberOfInfantArray objectAtIndex:row];
+//    }
+//
+//    return stringForTitle;
+//}
+
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+
     NSString * stringForTitle;
     if (component == 0) {
         stringForTitle = [self.numberOfAdultArray objectAtIndex:row];
@@ -227,29 +242,14 @@ int alreadySelectedComponent = 0;
     }else if (component == 2) {
         stringForTitle = [self.numberOfInfantArray objectAtIndex:row];
     }
-    
-    return stringForTitle;
+
+    UILabel * label = [[UILabel alloc] init];
+    label.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:23.0];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = stringForTitle;
+    return label;
+
 }
-
-
-//- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
-//
-//    NSString * stringForTitle;
-//    if (component == 0) {
-//                stringForTitle = [self.numberOfAdultArray objectAtIndex:row];
-//    }else if (component == 1) {
-//               stringForTitle =  [self.numberOfChildArray objectAtIndex:row];
-//    }else if (component == 2) {
-//                stringForTitle = [self.numberOfInfantArray objectAtIndex:row];
-//    }
-//
-//    UILabel * label = [[UILabel alloc] init];
-//    label.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:23.0];
-//    label.textAlignment = NSTextAlignmentCenter;
-//    label.text = stringForTitle;
-//    return label;
-//
-//}
 
 //MARK:- BOTTOM ANIMATIONS
 
