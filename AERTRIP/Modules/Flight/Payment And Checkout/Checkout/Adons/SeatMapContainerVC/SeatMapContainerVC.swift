abfcade8462aff0c6c1e4033aa94334f4de09ce9
Rishabh <rishabh.nautiyal@appinventiv.com>
2021-06-17 20:04:15 +0530

    Seats for dark mode in progress

--- AERTRIP/Modules/Flight/Payment And Checkout/Checkout/Adons/SeatMapContainerVC/SeatMapContainerVC.swift
+++ AERTRIP/Modules/Flight/Payment And Checkout/Checkout/Adons/SeatMapContainerVC/SeatMapContainerVC.swift
@@ -29,8 +29,8 @@ class SeatMapContainerVC: UIViewController {
     @IBOutlet weak var topNavBarView: TopNavigationView!
     @IBOutlet weak var seatMapContainerView: UIView!
     @IBOutlet weak var planeLayoutView: UIView!
-    @IBOutlet weak var planeLayoutTopSeparatorView: UIView!
-    @IBOutlet weak var planeLayoutBottomSeparatorView: UIView!
+    @IBOutlet weak var planeLayoutTopSeparatorView: ATDividerView!
+    @IBOutlet weak var planeLayoutBottomSeparatorView: ATDividerView!
     @IBOutlet weak var planeLayoutScrollView: UIScrollView!
     @IBOutlet weak var planeLayoutScrollContentView: UIView!
     @IBOutlet weak var planeShadowView: UIView!
@@ -168,9 +168,10 @@ class SeatMapContainerVC: UIViewController {
     }
     
     private func setupViews() {
+        view.backgroundColor = AppColors.themeWhiteDashboard
         setupApiIndicatorView()
-        planeLayoutTopSeparatorView.backgroundColor = AppColors.themeGray214
-        planeLayoutBottomSeparatorView.backgroundColor = AppColors.themeGray214
+//        planeLayoutTopSeparatorView.backgroundColor = AppColors.themeGray214
+//        planeLayoutBottomSeparatorView.backgroundColor = AppColors.themeGray214
 //        totalSeatAmountTopSeparatorView.backgroundColor = AppColors.themeGray214
         seatTotalTitleLbl.text = LocalizedString.seatTotal.localized
         seatTotalTitleLbl.font = AppFonts.Regular.withSize(12)
